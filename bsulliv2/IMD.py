ad872a7d0ed4bec148a133d6a65a10002f462f6b
bsulliv2 <Ben.Sullivan@analog.com>
2017-10-27 15:26:58 -0400

    First IMD Draft

--- bsulliv2/IMD.py
+++ bsulliv2/IMD.py
@@ -12,34 +12,6 @@ date = time.ctime(time.time())
 date = date.replace(':', '.')
 fh = open('Intermod_Dist_' + date + '.csv', 'w')
 
-# Source1.__SetState__(1)
-# Source2.__SetState__(1)
-# print Source1.__GetState__()
-# print Source2.__GetState__()
-# time.sleep(0.5)
-# Source1.__SetState__(0)
-# Source2.__SetState__(0)
-# print Source1.__GetState__()
-# print Source2.__GetState__()
-
-# Analyzer.__StoreState__('BS_Test_State')
-# time.sleep(1)
-# Analyzer.__LoadState__('BS_Test_State')
-# Source1.__SetFreq__(3.999e9)
-# Source2.__SetFreq__(4.001)
-# print Source1.__GetFreq__()
-
-# print Analyzer.__Setfc__(4.03e9)
-# Analyzer.__SetMarkerFreq__(1, 4.003e9)
-# print Analyzer.__GetMarkerAmp__(1)
-# Analyzer.__MarkerMax__(1)
-# print Analyzer.__GetSpan__()
-# print Analyzer.__SetSpan__(10e9)
-# print Analyzer.__Setfc__(4.03e9)
-# Analyzer.__LoadState__('BS_Test_State')
-
-# Analyzer.__LoadState__('BS_Test_State')
-
 freqlist = [100e6, 250e6, 500e6, 1e9, 4e9]
 lowerPeak = []
 higherPeak = []
@@ -66,3 +38,9 @@ for i in range(11):
     fh.write(str(lowerPeak).strip('[]'))
     fh.write('\n')
     fh.write(str(higherPeak).strip('[]'))
+    fh.write('\n')
+
+    print lowerPeak
+    lowerPeak = []
+    higherPeak = []
+
