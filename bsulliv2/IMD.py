eaeb72b666223b4592a59a85e3881d0d2ffeb565
bsulliv2 <Ben.Sullivan@analog.com>
2017-10-31 15:31:57 -0400

    Same as previous

--- bsulliv2/IMD.py
+++ bsulliv2/IMD.py
@@ -3,7 +3,7 @@ sys.path.append('../Common')
 from ADI_GPIB.AgilentN5181A import *
 from ADI_GPIB.AgilentN9030A import *
 
-# Supply = E3631A(23)
+Supply = E3631A(23)
 Source1 = AgilentN5181A(20)
 Source2 = AgilentN5181A(11)
 Analyzer = AgilentN9030A(18)
@@ -11,15 +11,27 @@ Analyzer = AgilentN9030A(18)
 date = time.ctime(time.time())
 date = date.replace(':', '.')
 fh = open('Intermod_Dist_' + date + '.csv', 'w')
+dut = '3-4'
+test = 'IMD3'
+equipment = 'N5181A N9030A BAL0026 BAL006'
+supplyV = Supply.__MeasP25V__()
+supplyI = Supply.__MeasP25I__()
+balun = 'INB: 0-VIN OUTB 0-VOP'
+header = (dut, date, test, equipment, supplyV, supplyI, balun)
+fh.write(header)
+fh.write('\n')
 
-freqlist = [100e6, 250e6, 500e6, 1e9, 4e9]
+freqlist = [100e6, 250e6, 500e6, 1e9, 1.5e9, 2e9, 2.5e9, 3e9, 3.5e9, 4e9]
 lowerPeak = []
 higherPeak = []
 lowCarrier = []
 highCarrier = []
+higherSourceAmp = []
+lowerSourceAmp = []
 
 Analyzer.__SetSpan__(10e3)
-for i in range(1):
+Analyzer.__SetAverage__(100)
+for i in range(10):
     for freq in freqlist:
         # Set sources
         Source1.__SetState__(0)
@@ -31,41 +43,50 @@ for i in range(1):
         Analyzer.__Setfc__(freq-1e6)
         Analyzer.__SetMarkerFreq__(1, freq-1e6)
         Source1.__SetState__(1)
-        time.sleep(1)
+        Analyzer.__ClearAverage__()
+        time.sleep(2)
         carrierMag = float(Analyzer.__GetMarkerAmp__(1))
         while abs(carrierMag - -8) >= 0.1:
             sourceAmp = float(Source1.__GetAmp__())
             Source1.__SetAmp__(sourceAmp + (-8 - carrierMag))
-            time.sleep(1)
+            Analyzer.__ClearAverage__()
+            time.sleep(2)
             carrierMag = float(Analyzer.__GetMarkerAmp__(1))
         # time.sleep(1)
         lowCarrier.append(float(Analyzer.__GetMarkerAmp__(1)))
+        lowerSourceAmp.append(float(Source1.__GetAmp__))
 
         # Repeat for high f carrier
         Source1.__SetState__(0)
         Analyzer.__Setfc__(freq+1e6)
         Analyzer.__SetMarkerFreq__(1, freq+1e6)
         Source2.__SetState__(1)
-        time.sleep(1)
+        Analyzer.__ClearAverage__()
+        time.sleep(2)
         carrierMag = float(Analyzer.__GetMarkerAmp__(1))
         while abs(carrierMag - -8) >= 0.1:
             sourceAmp = float(Source2.__GetAmp__())
             Source2.__SetAmp__(sourceAmp + (-8 - carrierMag))
-            time.sleep(1)
+            Analyzer.__ClearAverage__()
+            time.sleep(2)
             carrierMag = float(Analyzer.__GetMarkerAmp__(1))
         # time.sleep(1)
         highCarrier.append(float(Analyzer.__GetMarkerAmp__(1)))
+        higherSourceAmp.append(float(Source2.__GetAmp__))
 
+        # raw_input('Waiting...')
         Source1.__SetState__(1)
         # Measure IMD
         Analyzer.__Setfc__(freq-3e6)
         Analyzer.__SetMarkerFreq__(1, freq-3e6)
         # raw_input('Waiting...')
-        time.sleep(1)
+        Analyzer.__ClearAverage__()
+        time.sleep(2)
         lowerPeak.append(float(Analyzer.__GetMarkerAmp__(1)))
         Analyzer.__Setfc__(freq+3e6)
         Analyzer.__SetMarkerFreq__(1, freq+3e6)
-        time.sleep(1)
+        Analyzer.__ClearAverage__()
+        time.sleep(2)
         higherPeak.append(float(Analyzer.__GetMarkerAmp__(1)))
 
 
@@ -106,4 +127,5 @@ for i in range(1):
     higherPeak = []
     lowCarrier = []
     highCarrier = []
-
+    higherSourceAmp = []
+    lowerSourceAmp = []
